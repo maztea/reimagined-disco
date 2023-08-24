b29f70fb471297a2c6c4d8851b27a8a97991a549
maztea <maz.dimas@gmail.com>
2023-08-24 06:17:35 +0000

    use default bearer token authorization instead of custom header x-auth-token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,11 +4,13 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'your-secret-key'; // Replace with your actual secret key
 
 module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token'); // Assuming token is sent in the 'x-auth-token' header
-  
-  if (!token) {
+  const authHeader = req.header('Authorization');
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
   }
+
+  const token = authHeader.substring(7); // Remove 'Bearer ' from the header value
 
   try {
     const decoded = jwt.verify(token, secretKey);
